26bb26be490149d2d0c097cd36ef60ec65f82546
Alexey Ten <virtuoso_ten@mail.ru>
2022-11-17 17:58:45 +0300

    Created samples Base, Header,Footer

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    return render(request, template)
 
 
 def group_posts_list(request):
