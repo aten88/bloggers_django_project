109ad85a9d10bd8a334288ee554a1f945953e58c
Алексей Тен <virtuoso_ten@mail.ru>
2022-11-18 23:29:37 +0300

    Added links with NAMESPACE and NAME in URLS and VIEWS

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -2,10 +2,10 @@
 from django.urls import path
 
 from . import views
-
+app_name = 'posts'
 urlpatterns = [
-    path('', views.index),
-    path('group/', views.group_posts_list),
+    path('', views.index, name='index'),
+    path('group/', views.group_posts_list, name='group_posts_list'),
     path(
         'group/<slug:slug>/',
         views.group_posts
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -19,6 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     # импорт правил из приложения posts
-    path('', include('posts.urls')),
+    path('', include('posts.urls', namespace='posts')),
     path('admin/', admin.site.urls),
 ]
